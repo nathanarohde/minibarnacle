57d235f377de59a95d3e3ed3106c204f735e9e37
Monty Rohde <nathanarohde@gmail.com>
2015-08-14 13:09:11 -0700

    Successfully scrapes all subscribers from subscriber list.

--- minibarnacle.rb
+++ minibarnacle.rb
@@ -30,67 +30,23 @@ class Bot
 
   end_page_node = doc.css('div.g-pagination-wrap/a.mln')
   end_page = end_page_node.text
-  subscriber_nodes = doc.css('li.ib/a.thumb-wrap')
 
-  i = 1
-  while i <= end_page
-    # doc = Nokogiri::HTML(open("http://tapastic.com/swallowsofdoom/subscribers?pageNumber=10&series_id=1237"))
+  i=1
+  while i <= end_page.to_i
+    doc = Nokogiri::HTML(open("http://tapastic.com/swallowsofdoom/subscribers?pageNumber=" + i.to_s + "&series_id=1237"))
+    subscriber_nodes = doc.css('li.ib/a.thumb-wrap')
     subscriber_nodes.each do |subscriber_node|
-      subscriber_name = subscriber_node['href']
-      subscriber_url = BASE_SUBSCRIBER_URL + subscriber_name
-      subscribers << Subscriber.new(name: subscriber_name, url: subscriber_url)
-    end
+       subscriber_name = subscriber_node['href']
+       subscriber_url = BASE_SUBSCRIBER_URL + subscriber_name
+       subscribers << Subscriber.new(name: subscriber_name, url: subscriber_url)
+     end
+     puts i
+     sleep 0.5
+     i +=1
+   end
 
-    sleep 0.5
-    i +=1
-  end
-
-  a = page.at('a[title="Next page"]')
-  page = Nokogiri::HTML(open(a[:href]))
-
-  end_page = end_page_node.text
-
-  subscribers.each do |subscriber|
-    puts subscriber.name
-  end
+   subscribers.each do |subscriber|
+     puts subscriber.name
+   end
+   puts subscribers.length
 end
-
-  # categories.each do |c|
-  #   puts "Retrieving category info for #{c.name}"
-  #   doc = Nokogiri::HTML(open(c.url))
-  #   comic_nodes = doc.css('li.page-item-wrap.singular-item/a.title')
-  #
-  #   comic_nodes.each do |comic_node|
-  #       title = comic_node.text
-  #       comic_path = comic_node['href']
-  #       url = BASE_COMIC_URL + comic_path
-  #       comics << Comic.new(genre: c.name, name: title, url: url)
-  #   end
-  #
-  #   sleep 0.5
-  # end
-  #
-  # comics.each do |c|
-  #   puts "Retrieving comic info for #{c.name}"
-  #   doc = Nokogiri::HTML(open(c.url))
-  #   view_count_node = doc.css('span.cnt-txt')[0]
-  #   view_count = view_count_node.text.strip
-  #
-  #   follower_count_node = doc.css('span.cnt-txt')[1]
-  #   follower_count = follower_count_node.text.strip
-  #
-  #   description_node = doc.css('span#series-desc-body')[0]
-  #   description = description_node.text.strip.gsub(/\s{2,}/,'')
-  #
-  #   author_node = doc.css('span[@itemprop="name"]')[0]
-  #   author = author_node.text.strip
-  #
-  #   c.view_count = view_count
-  #   c.follower_count = follower_count
-  #   c.description = description
-  #   c.author = author
-  #
-  #   sleep 0.5
-  # end
-
-# end
